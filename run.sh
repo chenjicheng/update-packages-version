1d5162b0ba43bbdda69e85b86422f8468ac89485
Chen Jicheng <git@chenjicheng.com>
2022-05-22 01:06:50 +0800

    test

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ git config --global user.email "git@chenjicheng.com"
 if [[ -n $(cat repolist.txt) ]]; then
 	while IFS=',' read -r line; do
 		dir=basename $line .git
-		git clone $line
+		git clone $dir
 		cd $dir
 		docker run --rm -e EXPORT_SRC=1 -v $PWD:/pkg ghcr.io/chenjicheng/docker-makepkg:main
 		git commit -am "Update version"
