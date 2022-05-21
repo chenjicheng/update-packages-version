6fe7c1fe9db049777cd031b8e19d78231a0dfa66
Chen Jicheng <git@chenjicheng.com>
2022-05-22 00:24:01 +0800

    first commit

---
+++ run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+mkdir ~/.ssh
+cp config ~/.ssh/config
+
+ssh -o "StrictHostKeyChecking no" -T aur@aur.archlinux.org
+
+git config --global user.name "Github Actions"
+git config --global user.email "git@chenjicheng.com"
+
+if [[ -n $(cat repolist.txt) ]]; then
+	while IFS=',' read -r line; do
+		dir=basename $line .git
+		git clone $line
+		cd $dir
+		docker run --rm -e EXPORT_SRC=1 -v $PWD:/pkg ghcr.io/chenjicheng/docker-makepkg:main
+		git commit -am "Update version"
+		git push
+		cd ..
+		rm -rf $dir
+	done < repolist.txt
+fi
